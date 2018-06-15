fae992f7146d7f86e569eab1eaf971c1b1b70ff9
3131895796@qq.com <3131895796Xu>
2018-06-15 14:18:31 +0800

    chart 标记

--- src/app/echarts/echarts.component.ts
+++ src/app/echarts/echarts.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { Http } from '@angular/http';
+import {Component, OnInit} from '@angular/core';
+import {Http} from '@angular/http';
+import {NgxEchartsService} from "ngx-echarts";
 
 @Component({
   selector: 'app-echarts',
@@ -9,14 +10,82 @@ import { Http } from '@angular/http';
 export class EchartsComponent implements OnInit {
 
   showloading: boolean = true;
+  public ZheJiangOpt;
+  public data = [
+    {name: '丽水市', value: [119.49105224609375, 28.24937385955666, 2], img: 's-success.png'},
+    {name: '杭州市', value: [107.615944, 27.479744, 2], img: 's-success.png'},
+    {name: '温州市', value: [107.615944, 27.479744, 2], img: 's-success.png'},
+    {name: '宁波市', value: [107.615944, 27.479744, 2], img: 's-success.png'},
+    {name: '舟山市', value: [107.615944, 27.479744, 2], img: 's-success.png'},
+    {name: '台州市', value: [107.615944, 27.479744, 2], img: 's-success.png'},
+    {name: '金华市', value: [119.63150024414061, 29.07957590059833, 2], img: 's-success.png'},
+    {name: '衢州市', value: [118.84597778320312, 28.96369333786114, 2], img: 's-success.png'},
+    {name: '绍兴市', value: [107.615944, 27.479744, 2], img: 's-success.png'},
+    {name: '嘉兴市', value: [107.615944, 27.479744, 2], img: 's-success.png'},
+    {name: '湖州市', value: [107.615944, 27.479744, 2], img: 's-success.png'}
+  ]
 
-  constructor(public http: Http) {
+
+  constructor(public http: Http, private es: NgxEchartsService) {
     setTimeout(() => {
       this.showloading = false;
     }, 3000);
   }
 
   ngOnInit() {
+    let _data = this.data;
+
+    this.http.get('../assets/33.json').subscribe(result => {
+      this.es.registerMap('浙江', result['_body']);
+      this.ZheJiangOpt = {
+        title: {
+          text: '浙江省地图',
+          subtext: '',
+          sublink: '',
+          left: 'center'
+        },
+        series: [
+          {
+            type: 'custom',
+            coordinateSystem: 'geo',
+            renderItem: function (params, api) {
+              return {
+                type: 'image',
+                style: {
+                  image: "../assets/s-success.png",
+                  x: api.coord([
+                    _data[params.dataIndex].value[0], _data[params.dataIndex]
+                      .value[1]
+                  ])[0],
+                  y: api.coord([
+                    _data[params.dataIndex].value[0], _data[params.dataIndex]
+                      .value[1]
+                  ])[1]
+                }
+              };
+            },
+            data: _data
+          }
+        ],
+        geo: {
+          map: '浙江',
+          roam: true,
+          zoom: 1,
+          itemStyle: {
+            normal: {
+              label: {show: true},
+              // borderColor: '#387ba7',
+              // shadowColor: 'rgba(0, 0, 0, 0.5)',
+              // shadowBlur: 10,
+              // shadowOffsetX: 10
+            },
+            emphasis: {
+              // areaColor: '#b3f3f3'
+            }
+          }
+        }
+      };
+    });
   }
 
   chartOption = {
@@ -57,28 +126,28 @@ export class EchartsComponent implements OnInit {
         name: '邮件营销',
         type: 'line',
         stack: '总量',
-        areaStyle: { normal: {} },
+        areaStyle: {normal: {}},
         data: [120, 132, 101, 134, 90, 230, 210]
       },
       {
         name: '联盟广告',
         type: 'line',
         stack: '总量',
-        areaStyle: { normal: {} },
+        areaStyle: {normal: {}},
         data: [220, 182, 191, 234, 290, 330, 310]
       },
       {
         name: '视频广告',
         type: 'line',
         stack: '总量',
-        areaStyle: { normal: {} },
+        areaStyle: {normal: {}},
         data: [150, 232, 201, 154, 190, 330, 410]
       },
       {
         name: '直接访问',
         type: 'line',
         stack: '总量',
-        areaStyle: { normal: {} },
+        areaStyle: {normal: {}},
         data: [320, 332, 301, 334, 390, 330, 320]
       },
       {
@@ -91,7 +160,7 @@ export class EchartsComponent implements OnInit {
             position: 'top'
           }
         },
-        areaStyle: { normal: {} },
+        areaStyle: {normal: {}},
         data: [820, 932, 901, 934, 1290, 1330, 1320]
       }
     ]
@@ -125,9 +194,9 @@ export class EchartsComponent implements OnInit {
           }
         },
         data: [
-          { value: 335, name: '直达', selected: true },
-          { value: 679, name: '营销广告' },
-          { value: 1548, name: '搜索引擎' }
+          {value: 335, name: '直达', selected: true},
+          {value: 679, name: '营销广告'},
+          {value: 1548, name: '搜索引擎'}
         ]
       },
       {
@@ -136,19 +205,18 @@ export class EchartsComponent implements OnInit {
         radius: ['40%', '55%'],
 
         data: [
-          { value: 335, name: '直达' },
-          { value: 310, name: '邮件营销' },
-          { value: 234, name: '联盟广告' },
-          { value: 135, name: '视频广告' },
-          { value: 1048, name: '百度' },
-          { value: 251, name: '谷歌' },
-          { value: 147, name: '必应' },
-          { value: 102, name: '其他' }
+          {value: 335, name: '直达'},
+          {value: 310, name: '邮件营销'},
+          {value: 234, name: '联盟广告'},
+          {value: 135, name: '视频广告'},
+          {value: 1048, name: '百度'},
+          {value: 251, name: '谷歌'},
+          {value: 147, name: '必应'},
+          {value: 102, name: '其他'}
         ]
       }
     ]
   }
-
 
 
   linkoption = {
@@ -193,7 +261,7 @@ export class EchartsComponent implements OnInit {
     },
     xAxis: [{
       type: 'category',
-      data: [21231,1212,21231,3213],
+      data: [21231, 1212, 21231, 3213],
       axisTick: {
         alignWithLabel: true
       },
@@ -215,22 +283,22 @@ export class EchartsComponent implements OnInit {
           show: true
         }
       },
-      data:[21231,1212,21231,3213]
+      data: [21231, 1212, 21231, 3213]
     }]
   }
 
 
   datamapvalue = [
-    {name: '海门', value: [121.15,31.89,9]},
-    {name: '鄂尔多斯', value: [109.781327,39.608266,12]},
-    {name: '招远', value: [120.38,37.35,12]},
-    {name: '舟山', value: [122.207216,29.985295,12]},
-    {name: '齐齐哈尔', value: [123.97,47.33,14]},
-    {name: '盐城', value: [120.13,33.38,15]},
-    {name: '赤峰', value: [118.87,42.28,16]},
-    {name: '青岛', value: [120.33,36.07,18]},
-    {name: '乳山', value: [121.52,36.89,18]},
-    {name: '金昌', value: [102.188043,38.520089,19]}
+    {name: '海门', value: [121.15, 31.89, 9]},
+    {name: '鄂尔多斯', value: [109.781327, 39.608266, 12]},
+    {name: '招远', value: [120.38, 37.35, 12]},
+    {name: '舟山', value: [122.207216, 29.985295, 12]},
+    {name: '齐齐哈尔', value: [123.97, 47.33, 14]},
+    {name: '盐城', value: [120.13, 33.38, 15]},
+    {name: '赤峰', value: [118.87, 42.28, 16]},
+    {name: '青岛', value: [120.33, 36.07, 18]},
+    {name: '乳山', value: [121.52, 36.89, 18]},
+    {name: '金昌', value: [102.188043, 38.520089, 19]}
   ];
 
 
