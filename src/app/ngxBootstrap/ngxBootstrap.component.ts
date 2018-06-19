a60be0523ec27d6a3bfea0922d26f3a92b7a7626
3131895796@qq.com <3131895796Xu>
2018-06-19 18:59:47 +0800

    error

--- src/app/ngxBootstrap/ngxBootstrap.component.ts
+++ src/app/ngxBootstrap/ngxBootstrap.component.ts
@@ -6,13 +6,26 @@ import {Http} from '@angular/http';
   templateUrl: './ngxBootstrap.component.html'
 })
 export class NgxBootstrapComponent implements OnInit {
+  public masterList = ["a", "b", "c", "d", "e", "f", "g", "h", "i"];
+  public items = [];
 
+  // Pagination
+  public currentPage: number = 1; // # of current page to show
+  public itemsPerPage: number = 3; // # of items per page
+  public maxSize: number = 3; // max # of page buttons to show at once
+  public totalItems: number; // total # of items
   constructor(public http: Http) {
+    this.onPageChange({page: this.currentPage, itemsPerPage: this.itemsPerPage});
   }
 
   ngOnInit() {
   }
 
+  onPageChange(page) {
+    let start = (page.page - 1) * page.itemsPerPage;
+    let end = page.itemsPerPage > -1 ? (start + page.itemsPerPage) : this.masterList.length;
+    this.items = this.masterList.slice(start, end);
+  }
 
 
 }
--- src/app/ngxBootstrap/ngxBootstrap.module.ts
+++ src/app/ngxBootstrap/ngxBootstrap.module.ts
@@ -1,9 +1,11 @@
 
 import { NgModule } from '@angular/core/src/metadata/ng_module';
 import { NgxBootstrapComponent } from './ngxBootstrap.component';
+import { ButtonsModule } from 'ngx-bootstrap';
 
 @NgModule({
   imports: [
+    ButtonsModule.forRoot()
   ],
   declarations: [NgxBootstrapComponent],
 })
